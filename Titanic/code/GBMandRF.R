954caf0ba1f49ceb78d48b9dbdde335112bb3284
vivekanand <vivek8943@gmail.com>
2016-04-19 21:31:04 -0500

    removed unnecessary steps

--- Titanic/code/GBMandRF.R
+++ Titanic/code/GBMandRF.R
@@ -47,26 +47,7 @@ gbm1 <- h2o.gbm(
 summary(gbm1)                   ## View information about the model.
 
 h2o.hit_ratio_table(gbm1,valid = T)
-## Overall accuracy.
 
-## This default GBM is much worse than our original random forest.
-## The GBM is far from converging, so there are three primary knobs to adjust
-##  to get our performance up if we want to keep a similar run time.
-## 1: Adding trees will help. The default is 50.
-## 2: Increasing the learning rate will also help. The contribution of each
-##  tree will be stronger, so the model will move further away from the
-##  overall mean.
-## 3: Increasing the depth will help. This is the parameter that is the least
-##  straightforward. Tuning trees and learning rate both have direct impact
-##  that is easy to understand. Changing the depth means you are adjusting
-##  the "weakness" of each learner. Adding depth makes each tree fit the data
-##  closer. 
-##
-## The first configuration will attack depth the most, since we've seen the
-##  random forest focus on a continuous variable (elevation) and 40-class factor
-##  (soil type) the most.
-##
-## Also we will take a look at how to review a model while it is running.
 
 ###############################################################################
 gbm2 <- h2o.gbm(
@@ -85,13 +66,6 @@ gbm2 <- h2o.gbm(
   seed = 2000000)             ##
 
 
-
-## This has moved us in the right direction, but still lower accuracy 
-##  than the random forest.
-## And it still has not converged, so we can make it more aggressive.
-## We can now add the stochastic nature of random forest into the GBM
-##  using some of the new H2O settings. This will help generalize 
-##  and also provide a quicker runtime, so we can add a few more trees.
 
 gbm3 <- h2o.gbm(
   training_frame = train,     ##
@@ -117,19 +91,6 @@ h2o.hit_ratio_table(gbm2,valid = T)[1,2]    ## review the second model's accurac
 h2o.hit_ratio_table(gbm3,valid = T)[1,2]    ## review the newest model's accuracy
 ###############################################################################
 
-## Now the GBM is close to the initial random forest.
-## However, we used a default random forest. 
-## Random forest's primary strength is how well it runs with standard
-##  parameters. And while there are only a few parameters to tune, we can 
-##  experiment with those to see if it will make a difference.
-## The main parameters to tune are the tree depth and the mtries, which
-##  is the number of predictors to use.
-## The default depth of trees is 20. It is common to increase this number,
-##  to the point that in some implementations, the depth is unlimited.
-##  We will increase ours from 20 to 30.
-## Note that the default mtries depends on whether classification or regression
-##  is being run. The default for classification is one-third of the columns.
-##  The default for regression is the square root of the number of columns.
 
 rf2 <- h2o.randomForest(        ##
   training_frame = train,       ##
